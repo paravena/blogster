43f96d65d527dd0ea258cc8ba2cd5d11b53564da
Pablo Aravena <paravena74@gmail.com>
2018-07-02 16:52:35 -0400

    travis config

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://pablo:Pabloso8@ds117111.mlab.com:17111/blogdb_dev',
+  mongoURI: 'mongodb://127.0.0.1:27017/blogdb_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
