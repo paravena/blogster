abdedab4f8e2f5df269515184e2270c83288aaf0
Pablo Aravena <paravena74@gmail.com>
2018-07-01 21:59:32 -0400

    first commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://pablo:Pabloso8@ds117111.mlab.com:17111/blogdb_dev',
+  cookieKey: '123123123'
+};
